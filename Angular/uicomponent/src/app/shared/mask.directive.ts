22bee97c74e352d1f11dfe806535abd2b310c1e4
Naveen Kumar Sunkara <naveen241193@gmail.com>
2018-01-22 01:39:05 +0530

    summa

--- Angular/uicomponent/src/app/shared/mask.directive.ts
+++ Angular/uicomponent/src/app/shared/mask.directive.ts
@@ -1,10 +1,29 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, AfterViewInit } from '@angular/core';
 
 @Directive({
   selector: '[appMask]'
 })
-export class MaskDirective {
-
+export class MaskDirective implements AfterViewInit {
+  @Input() mask: string;
+  unmaskedValue: string;
+  dot = decodeURI("%E2%80%A2");
   constructor(private elRef: ElementRef) {}
-
+  @HostListener('blur') onblur() {
+    if(this.mask != "false"){
+      this.mask_all()
+    }
+  }
+  @HostListener('change') onchange(){
+    this.unmaskedValue = this.elRef.nativeElement.value;
+  }
+  @HostListener('focus') onfocus(){
+    this.elRef.nativeElement.value = this.unmaskedValue;
+  }
+  mask_all(): void{
+    let val = this.unmaskedValue;
+    val = val.replace(/./g, decodeURI("%E2%80%A2"));
+    this.elRef.nativeElement.value = val;
+    console.log(val)
+  }
+  ngAfterViewInit(): void {}
 }
